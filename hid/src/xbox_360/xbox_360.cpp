9e1eb451da1ad1b4733c4807abeb0c606bbc9b86
Daniel Packard <pack3754@gmail.com>
2013-07-06 19:15:14 -0600

    snap

--- hid/src/xbox_360/xbox_360.cpp
+++ hid/src/xbox_360/xbox_360.cpp
@@ -2,19 +2,22 @@
 #include "std_msgs/String.h"
 #include "xbox_360.h"
 
-void testCB(const std_msgs::String &f)
-{
-  ROS_INFO("here");
-}
-
 int main(int argc, char* argv[])
 {
   ros::init(argc, argv, "xbox_360");
   ros::NodeHandle n;
 
-  ros::Subscriber test = n.subscribe("joint_command",1, testCB);
+  ros::Publisher test = n.advertise<std_msgs::String>("chitchat", 1000);
+
+  ros::Rate loop_rate(10);
 
   ROS_INFO("xbox 360 node is under dev...");
-
-  ros::spin();
+  while(ros::ok())
+  {
+    std_msgs::String msg;
+    msg.data="hello";
+    test.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
 }
