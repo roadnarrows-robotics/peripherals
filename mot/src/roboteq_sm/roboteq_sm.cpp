2387ef1103f8907cf178b2cf5847fceceefa40e9
Jessica <jessica@speedbump.co.roadnarrows.com>
2013-07-08 17:09:41 -0600

    Added roboteq small ROS node

---
+++ mot/src/roboteq_sm/roboteq_sm.cpp
@@ -0,0 +1,47 @@
+// header, etc
+#include <string>
+#include "ros/ros.h"
+#include "mot/SetSpeed.h"
+#include "roboteq_sm.h"
+#include "rnr/mot/MotRoboteqSmall.h"
+
+using namespace std;
+
+bool SetSpeed(mot::SetSpeed::Request &req,
+              mot::SetSpeed::Response &rsp)
+{
+  ROS_INFO("Setting speed");
+    pMot->setSpeed(req.mot_id,req.speed);
+  return true;
+}
+
+int main(int argc, char* argv[])
+{
+  ros::init(argc, argv, "roboteq");
+  ros::NodeHandle n("roboteq");
+
+  pMot=new MotRoboteqSmall;
+
+  string devName="/dev/ttyACM0";
+  int baudRate=115200;
+  if(pMot->open(devName, baudRate)!=0)
+  {
+    fprintf(stderr, "jnt failed to open\n");
+    return -1;
+  }
+
+// TODO - create services, subscriptions, published topics, etc
+
+  ros::ServiceServer set_speed_ser = n.advertiseService("set_speed",
+                                                       SetSpeed);
+
+  ros::spin();
+
+  if(pMot->close()!=0)
+  {
+    fprintf(stderr, "jnt failed to close\n");
+    return -1;
+  }
+
+  return 0;
+}
--- mot/src/roboteq_sm/roboteq_sm.h
+++ mot/src/roboteq_sm/roboteq_sm.h
@@ -1,6 +1,13 @@
 #ifndef _PERIPHS_MOT_ROBOTEQ_H
 #define _PERIPHS_MOT_ROBOTEQ_H
 
+#include "rnr/mot/MotRoboteqSmall.h"
+
+using namespace rnr;
+
+MotRoboteqSmall *pMot;
+
+
 //TODO -- include roboteq headers, declare pMot global control interface
 
 #endif // _PERIPHS_MOT_ROBOTEQ_H
