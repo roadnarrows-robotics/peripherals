17644f0e7210e7e08b0f1f3e3b842411b6d22d00
Jessica Trujillo <roadnarrows.jessica@gmail.com>
2013-07-18 15:25:42 -0600

    working on roboteq xbox control demo

---
+++ mot/src/roboteq_sm/roboteq_sm_Subscriptions.h
@@ -0,0 +1,25 @@
+#ifndef _ROBOTEQ_SM_SUBSCRITIONS_H
+#define _ROBOTEQ_SM_SUBSCRITIONS_H
+
+#include "ros/ros.h"
+#include "mot/SpeedCmd.h"
+#include "roboteq_sm.h"
+
+#include <stdio.h>
+
+void speed_commandCB(const mot::SpeedCmd &cmd)
+{ 
+  ROS_INFO("dhp -here cmd.speeds[0] = %d ",cmd.speeds[0] );
+  if(cmd.mot_ids.size() != cmd.speeds.size())
+  {
+    ROS_ERROR("Size of mot ids does not match size of speeds: %lu != %lu", 
+              cmd.mot_ids.size(), cmd.speeds.size());
+    return;
+  }
+
+  pMot->setSpeed(cmd.mot_ids[0],cmd.speeds[0]);
+
+}
+
+
+#endif // _ROBOTEQ_SM_SUBSCRITIONS_H
